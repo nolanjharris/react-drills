4d24ad1467654aacce6c0f1a25bc8c04dc353a24
nolanjharris <thenolanjames@gmail.com>
2019-07-02 13:44:50 -0500

    completed app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      list: ['Nolan', 'John', 'Alex', 'Garrett']
+    }
+  }
+
   render() {
+    let displayList = this.state.list.map(e => {
+      return <h2>{e}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {displayList}
       </div>
     );
   }
